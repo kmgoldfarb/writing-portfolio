e9af94d74ce22e43ff81fb23617b4fa9c9269433
Kevin <kmgoldfarb@gmail.com>
2021-04-08 19:37:26 -0400

    Testing CSS Modules
    Add one CSS file to test out CSS Modules in a component.

--- src/Components/PreviewCard/PreviewCard.js
+++ src/Components/PreviewCard/PreviewCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Auxiliary from '../../HOC/Auxiliary/Auxiliary';
+import classes from './PreviewCard.module.css';
 
 const PreviewCard = (props) => {
 
         return (
             <Auxiliary>
-                <div>{props.title}</div>
+                <div className={classes.title}>{props.title}</div>
                 <div>{props.description}</div>
                 <div>{props.date}</div>
             </Auxiliary>
