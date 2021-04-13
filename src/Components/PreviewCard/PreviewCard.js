55afa4dd5ade9644a8e9ef2f41367ce38981c370
Kevin <kmgoldfarb@gmail.com>
2021-04-12 21:31:15 -0400

    Added arrow on title cards
    Added arrow to make it easier to understand that you can click the titles of each post.

--- src/Components/PreviewCard/PreviewCard.js
+++ src/Components/PreviewCard/PreviewCard.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import classes from './PreviewCard.module.css';
 import Auxiliary from '../../HOC/Auxiliary/Auxiliary';
+import Image from '../../images/round-arrow-right.png';
 
 const PreviewCard = (props) => {
 
         return (
             <Auxiliary>
                 <Link to={'/posts/' + props.slug} className={classes.Link}>
-                    <div className={classes.title}>{props.title}</div>
+                    <div className={classes.TitleContainer}>
+                        <div className={classes.title}>{props.title}<img className={classes.arrow} src={Image} alt='right arrow'/></div>
+                    </div>
                 </Link>
                     <div className={classes.description}>{props.description}</div>
                     <div className={classes.date}>{props.date}</div>
