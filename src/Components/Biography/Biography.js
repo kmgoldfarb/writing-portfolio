e952e4c3e1ca77aae83ac0f4b381f78c5f6e8e6f
Kevin <kmgoldfarb@gmail.com>
2021-05-28 21:07:48 -0400

    Light Styling, Refreshing My Memory

--- src/Components/Biography/Biography.js
+++ src/Components/Biography/Biography.js
@@ -1,21 +1,29 @@
-import React from 'react';
-import classes from './Biography.module.css';
+import React from "react";
+import classes from "./Biography.module.css";
 
 function Biography(props) {
-    
-    const bodyHtmlText = props.body.html;
-    const subheadHtmlText = props.subhead.html;
-    console.log(subheadHtmlText);
+  const bodyHtmlText = props.body.html;
+  const subheadHtmlText = props.subhead.html;
+  console.log(subheadHtmlText);
 
-
-    return (
-        <div className={classes.profile}>
-            <div className={classes.name}>{props.name}</div>
-            <div className={classes.subhead} dangerouslySetInnerHTML={{__html: subheadHtmlText}} />
-            <div className={classes.body} dangerouslySetInnerHTML={{__html: bodyHtmlText}} />
-            <img alt='Photo of Daniella Goldfarb' src={props.image.url}/>
-        </div>
-    )
+  return (
+    <div className={classes.profile}>
+      <div className={classes.name}>{props.name}</div>
+      <div
+        className={classes.subhead}
+        dangerouslySetInnerHTML={{ __html: subheadHtmlText }}
+      />
+      <div
+        className={classes.body}
+        dangerouslySetInnerHTML={{ __html: bodyHtmlText }}
+      />
+      <img
+        className={classes.image}
+        alt="Photo of Daniella Goldfarb"
+        src={props.image.url}
+      />
+    </div>
+  );
 }
 
 export default Biography;
